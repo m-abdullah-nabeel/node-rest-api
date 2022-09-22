60de2c1f1c94e14a80c2ff7dc8f97126e881fc9a
Muhammad Abdullah Nabeel <baanwarapapiha@gmail.com>
2022-09-22 19:39:24 +0500

    added logging

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express()
+const morgan = require("morgan")
 
+app.use(morgan('dev'))
+// Routes
 const product_routes = require("./api/routes/products")
 const order_routes = require("./api/routes/orders")
 
