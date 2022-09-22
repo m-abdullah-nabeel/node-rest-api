5e839086e2dbad9e0ba487ae69658acf1bedd473
Muhammad Abdullah Nabeel <baanwarapapiha@gmail.com>
2022-09-22 20:41:32 +0500

    added error handling by next-error- need more attention later

--- app.js
+++ app.js
@@ -10,4 +10,19 @@ const order_routes = require("./api/routes/orders")
 app.use('/products', product_routes);
 app.use('/orders', order_routes);
 
+// Error Handling
+app.use((req, res, next) => {
+    const error = new Error("Not Found")
+    error.status(404)
+    next(error)
+})
+
+app.use((req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 module.exports = app;
